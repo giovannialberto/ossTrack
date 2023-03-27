b2639fe62d31cc4d261228d5e79a565b4091ba4d
Giovanni Alberto Falcione <giovannialbertofalcione@gmail.com>
2023-03-27 15:34:01 +0000

    fix: solved problems with docker and dash server

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,7 @@
 import os
 import argparse
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html, dcc
 import pandas as pd
 import plotly.graph_objs as go
 import sqlite3
@@ -162,4 +161,4 @@ app.layout = html.Div(children=[
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False, port=8050)
+    app.run(host='0.0.0.0', port=8050, debug=True)
