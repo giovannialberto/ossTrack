fa85c0da84dd72fe057737e71b09af68a4d78dc1
Giovanni Alberto Falcione <giovannialbertofalcione@gmail.com>
2023-04-08 08:52:58 +0000

    feat: added missing api endpoints

--- api/app.py
+++ api/app.py
@@ -45,10 +45,221 @@ def get_stars():
 # Endpoint to fetch number of forks for a date range
 @app.route('/forks')
 def get_forks():
-    # Similar to get_stars() function, but with different query and result format
-    pass
-
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
     
+    conn = sqlite3.connect(args.database_url)
+    c = conn.cursor()
+    
+    if start_date_str is None and end_date_str is None:
+        # By default, return last 30 days
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=30)
+    else:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    
+    query = f"SELECT date, forks FROM metrics WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    c.execute(query)
+    rows = c.fetchall()
+    
+    result = {
+        'data': [{
+            'date': row[0],
+            'value': row[1]
+        } for row in rows]
+    }
+    
+    conn.close()
+    return jsonify(result)
+
+# Endpoint to fetch forks to stars ratio for a date range
+@app.route('/forks-to-stars')
+def get_forks_to_stars():
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
+    
+    conn = sqlite3.connect(args.database_url)
+    c = conn.cursor()
+    
+    if start_date_str is None and end_date_str is None:
+        # By default, return last 30 days
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=30)
+    else:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    
+    query = f"SELECT date, forks_to_stars_ratio FROM metrics WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    c.execute(query)
+    rows = c.fetchall()
+    
+    result = {
+        'data': [{
+            'date': row[0],
+            'value': row[1]
+        } for row in rows]
+    }
+    
+    conn.close()
+    return jsonify(result)
+
+# Endpoint to fetch number of subscribers for a date range
+@app.route('/subscribers')
+def get_subscribers():
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
+    
+    conn = sqlite3.connect(args.database_url)
+    c = conn.cursor()
+    
+    if start_date_str is None and end_date_str is None:
+        # By default, return last 30 days
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=30)
+    else:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    
+    query = f"SELECT date, subscribers FROM metrics WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    c.execute(query)
+    rows = c.fetchall()
+    
+    result = {
+        'data': [{
+            'date': row[0],
+            'value': row[1]
+        } for row in rows]
+    }
+    
+    conn.close()
+    return jsonify(result)
+
+# Endpoint to fetch number of contributors for a date range
+@app.route('/contributors')
+def get_contributors():
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
+    
+    conn = sqlite3.connect(args.database_url)
+    c = conn.cursor()
+    
+    if start_date_str is None and end_date_str is None:
+        # By default, return last 30 days
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=30)
+    else:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    
+    query = f"SELECT date, contributors FROM metrics WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    c.execute(query)
+    rows = c.fetchall()
+    
+    result = {
+        'data': [{
+            'date': row[0],
+            'value': row[1]
+        } for row in rows]
+    }
+    
+    conn.close()
+    return jsonify(result)
+
+# Endpoint to fetch number of issues opened for a date range
+@app.route('/issues-opened')
+def get_issues_opened():
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
+    
+    conn = sqlite3.connect(args.database_url)
+    c = conn.cursor()
+    
+    if start_date_str is None and end_date_str is None:
+        # By default, return last 30 days
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=30)
+    else:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    
+    query = f"SELECT date, issues_opened FROM metrics WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    c.execute(query)
+    rows = c.fetchall()
+    
+    result = {
+        'data': [{
+            'date': row[0],
+            'value': row[1]
+        } for row in rows]
+    }
+    
+    conn.close()
+    return jsonify(result)
+
+# Endpoint to fetch number of issues closed for a date range
+@app.route('/issues-closed')
+def get_issues_closed():
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
+    
+    conn = sqlite3.connect(args.database_url)
+    c = conn.cursor()
+    
+    if start_date_str is None and end_date_str is None:
+        # By default, return last 30 days
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=30)
+    else:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    
+    query = f"SELECT date, issues_closed FROM metrics WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    c.execute(query)
+    rows = c.fetchall()
+    
+    result = {
+        'data': [{
+            'date': row[0],
+            'value': row[1]
+        } for row in rows]
+    }
+    
+    conn.close()
+    return jsonify(result)
+
+# Endpoint to fetch number of PRs merged for a date range
+@app.route('/prs-merged')
+def get_prs_merged():
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
+
+    conn = sqlite3.connect(args.database_url)
+    c = conn.cursor()
+
+    if start_date_str is None and end_date_str is None:
+        # By default, return last 30 days
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=30)
+    else:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+
+    query = f"SELECT date, pr_merged FROM metrics WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    c.execute(query)
+    rows = c.fetchall()
+
+    result = {
+        'data': [{
+            'date': row[0],
+            'value': row[1]
+        } for row in rows]
+    }
+
+    conn.close()
+    return jsonify(result)
+
+
 if __name__ == '__main__':
     # Parse command-line arguments
     args = parser.parse_args()
